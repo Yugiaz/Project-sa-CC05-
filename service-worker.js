2a500fd112f077567715b7108387b20b6c0a8872
Yugiaz <wwwminter2@gmail.com>
2025-06-25 10:11:09 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,14 +7,12 @@ const urlsToCache = [
   "./manifest.json"
 ];
 
-// Install SW and cache resources
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-// Serve cached resources when offline
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
